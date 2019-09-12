f914ac76c57dbbdb3bef501ad5a0605ac0e31eab
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-09-12 23:06:34 +0300

    checkpoint

--- src/manager/sync/__init__.py
+++ src/manager/sync/__init__.py
@@ -1,11 +1,12 @@
-from manager.db import mongo_db
 from manager.constants import *
+from manager.db import mongo_db
 from manager.db import session_maker
-from manager.model import Domain
 from manager.db_helper import set_project_since_time, get_project_since_time
-import logging
+from manager.helper import delete_cert
+from manager.logger import create_logger
+from manager.model import Domain
 
-logging.basicConfig(level=logging.INFO)
+logger = create_logger('sync')
 
 
 def get_project_list(since=None) -> list:
@@ -24,22 +25,30 @@ def main():
     projects = get_project_list(since_time)
 
     if len(projects) == 0:
-        logging.info('No new project')
         session.close()
         return
 
     for project in projects:
         if project['status'] == PROJECT_STATUS_ON:
-            # insert record to local db
             d = Domain()
             d.name = project['name']
             session.add(d)
-            pass
+
+            logger.info('New project {}'.format(project['name']))
 
         if project['status'] == PROJECT_STATUS_DELETED:
-            # delete cert
-            # delete record from local db
-            pass
+            logger.info('Project deleted {}'.format(project['name']))
+
+            d = session.query(Domain).filter(Domain.name == project['name'])
+
+            if d is not None:
+                logger.info('Project deleting {}'.format(project['name']))
+                session.delete(d)
+                logger.info('Record deleted {}'.format(project['name']))
+                delete_cert(project['name'])
+                logger.info('Certificate deleted {}'.format(project['name']))
+            else:
+                logger.info('Record not found {}'.format(project['name']))
 
         set_project_since_time(session, project['updatedAt'])
         session.flush()
