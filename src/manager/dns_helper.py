d3078fe67d74a1d87559f8c65ce2694cc0348d7c
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-02-15 15:46:20 +0300

    some typing

--- src/manager/dns_helper.py
+++ src/manager/dns_helper.py
@@ -4,7 +4,7 @@ from dns.resolver import Resolver, NXDOMAIN
 resolver = Resolver()
 
 
-def is_domain_exists(name):
+def is_domain_exists(name: str) -> bool:
     try:
         resolver.query(name, 'A')
     except NXDOMAIN:
@@ -15,7 +15,7 @@ def is_domain_exists(name):
     return True
 
 
-def get_txt_records(name):
+def get_txt_records(name: str) -> list:
     try:
         answers = resolver.query(name, 'TXT')
         txt_records = list(set([x.to_text().strip().rstrip('"').lstrip('"') for x in answers]))
